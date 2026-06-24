3a4b1dc5ca2ece58cd74a4f956d945f842df17cf
Haokai Dai <haokaidai.zju@gmail.com>
2026-06-24 15:56:03 +0800

    fix IMU degree bug

--- src/lite3_transfer/interface/robot/udp_interface.hpp
+++ src/lite3_transfer/interface/robot/udp_interface.hpp
@@ -268,7 +268,7 @@ public:
     }
 
     virtual Vec3f GetImuRpy() {
-        rpy_ << robot_data_->imu.angle_roll/180.*M_PI, robot_data_->imu.angle_pitch/180.*M_PI, robot_data_->imu.angle_yaw/180.*M_PI;
+        rpy_ << robot_data_->imu.angle_roll, robot_data_->imu.angle_pitch, robot_data_->imu.angle_yaw;
         return rpy_;
     }
 
@@ -357,4 +357,3 @@ public:
         imu_data_pub_->publish(msg);
     }
 };
-
